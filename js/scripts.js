1cc1d87d395b07db4265761b147a7df28f933e00
Vasiliy <you@example.comvasiliy.kostynian@mail.ru>
2017-10-04 17:40:43 +0300

    new page registr

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,16 @@ $(document).ready(function(e) {
 
 
 
+   jQuery('input, select').focus(function(){
+     jQuery(this).data('placeholder',jQuery(this).attr('placeholder'))
+     jQuery(this).attr('placeholder','');
+     jQuery(this).parent().prevAll( '.span-title' ).css("opacity","1");
+   });
+   jQuery('input, select').blur(function(){
+     jQuery(this).attr('placeholder',jQuery(this).data('placeholder'));
+     jQuery(this).parent().prevAll( '.span-title' ).css("opacity","0");
+     
+   });
+
 
 });
